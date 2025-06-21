731c997e97c2c2ebbf16c5b20e7b5e87ce097fde
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-21 11:26:36 +0600

    admin update  js   🛟

--- src/pages/Admin-Update.jsx
+++ src/pages/Admin-Update.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import { useAuth } from "../store/auth";
 import { toast } from "react-toastify";
 import { useEffect, useState } from "react";
-
+// admin update
 export const AdminUpdate = () => {
     const params = useParams();
     const [data, setData] = useState({
